d7ce7ead51e3db8b5d858bbf573c668412b287e9
bernardogeinserph-collab <bernardo.geinserph@gmail.com>
2026-06-12 17:01:25 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,4 @@ import{BrowserRouter}from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>)
+// v1.0.1
