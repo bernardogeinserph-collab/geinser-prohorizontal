3941ed2057df99e7ca5d472826089fe5949d750b
bernardogeinserph-collab <bernardo.geinserph@gmail.com>
2026-06-12 13:54:46 -0500

    feat: agregar tailwind.config.js con colores corporativos Geinser

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        geinser: {
+          blue: '#1e6fae',
+          dark: '#0d2d4a',
+          gold: '#c9a227',
+          light: '#e8f4fd'
+        }
+      }
+    }
+  },
+  plugins: []
+}
