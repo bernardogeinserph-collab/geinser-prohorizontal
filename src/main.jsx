d7c4f7754f7e2c04f6ea0121d8d5471b1cb285ec
bernardogeinserph-collab <bernardo.geinserph@gmail.com>
2026-06-15 13:36:59 -0500

    fix: quitar doble BrowserRouter que crasheaba React

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import{BrowserRouter}from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>)
-// v1.0.1
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
